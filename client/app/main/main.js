53bd7edb3fb5236d9bb462a82d212afbe4fba3bc
gail <gsteiger@netvision.net.il>
2015-08-31 23:42:32 +0300

    initial commit

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('fsauthApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
